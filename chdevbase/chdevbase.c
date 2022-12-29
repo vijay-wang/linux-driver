4281244a6377630e197a9b1b65cd397cffaeb834
vijay-wang <ww107587@gmail.com>
2022-12-29 21:46:43 +0800

    add chdevbase/chdevbase.c chdevbase/chdevbaseApp.c

--- chdevbase/chdevbase.c
+++ chdevbase/chdevbase.c
@@ -9,7 +9,6 @@
 #define CHDEVBASE_MAJOR 200
 #define CHDEVBASE_NAME "chdevbase" 
 
-static char readbuf[100];
 static char writebuf[100];
 static char kernel_data[] = {"kernel data!"};
 
@@ -18,32 +17,32 @@ static int open_chdevbase(struct inode *inode, struct file *filp)
 	return 0;
 }
 
-static size_t read_chdevbase(struct file *filp, char __user *buf,
+static ssize_t read_chdevbase(struct file *filp, char __user *buf,
 		size_t cnt, loff_t *offt)
 {
-	memcpy(readbuf, kernel_data, sizeof(kernel_data));
-	if(copy_to_user(buf, kernel_data, cnt)) {
+	if(copy_to_user(buf, kernel_data, sizeof(kernel_data))) {
 		printk(KERN_ERR "chdevbase read faild!\r\n");
-		return -EFAULT;
+		return -1;
 	}
 	else
-		printk(KERN_INFO "chdevbase read %u byte(s) : %s\r\n", sizeof(kernel_data), readbuf);
+		printk(KERN_INFO "chdevbase read %u byte(s) : %s\r\n", sizeof(kernel_data), kernel_data);
 
-	return sizeof(kernel_data));
+	return sizeof(kernel_data);
 }
 
-static size_t write_chdevbase(struct file *filp, const char __user *buf,
+static ssize_t write_chdevbase(struct file *filp, const char __user *buf,
 		size_t cnt, loff_t *offt)
 {
+	memset(writebuf, 0, sizeof(writebuf));
 	if(copy_from_user(writebuf, buf, cnt)) {
 		
 		printk(KERN_ERR "chdevbase write faild!\r\n");
-		return -EFAULT;
+		return -1;
 	}
 	else
 		printk(KERN_INFO "chdevbase kernel rcv %u byte(s) : %s\r\n", cnt, writebuf);
 
-	return cnt
+	return cnt;
 }
 
 static int release_chdevbase(struct inode *inode, struct file *filp)
@@ -51,7 +50,7 @@ static int release_chdevbase(struct inode *inode, struct file *filp)
 	return 0;
 }
 
-static struct file_operation chdevbase_fops = {
+static struct file_operations chdevbase_fops = {
 	.owner = THIS_MODULE,
 	.open = open_chdevbase,
 	.release = release_chdevbase,
---
+++ chdevbase/chdevbaseApp.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	char readbuf[16] = {0};
+	char writebuf[16] = {0};
+	ssize_t read_size;
+	ssize_t write_size;
+
+	fd = open("/dev/chdevbase", O_RDWR);
+
+	if (fd == -1) {
+		perror("open");
+		return -1;
+	}
+	
+	if (!strcmp(argv[1], "read")) {
+
+		read_size = read(fd, readbuf, sizeof(readbuf));
+		
+		if (read_size == -1) {
+			perror("read");
+			return -1;	
+		}
+
+		printf("read %ld byte(s):%s\n", read_size, readbuf);
+	
+	}
+
+
+	if (!strcmp(argv[1], "write")) {
+		memcpy(writebuf, argv[2], strlen(argv[2]));
+
+		write_size = write(fd, argv[2], strlen(argv[2]));
+		
+		if (write_size == -1) {
+			perror("write");
+			return -1;	
+		}
+
+		printf("write %ld byte(s):%s\n", write_size, writebuf);
+	}
+
+
+	close(fd);
+
+	return 0;
+}
+
