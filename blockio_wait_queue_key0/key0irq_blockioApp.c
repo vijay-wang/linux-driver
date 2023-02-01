f1b9d35f7b1e19f6388e269422df564d10bd2b0f
vijay-wang <ww107587@gmail.com>
2023-02-01 15:30:44 +0800

    add blockio_wait_queue_key0

---
+++ blockio_wait_queue_key0/key0irq_blockioApp.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <time.h>
+
+
+int main(int argc, char **argv)
+{
+	int fd;
+	char readbuf[16] = {0};
+	char writebuf[16] = {0};
+	ssize_t read_size;
+	ssize_t write_size;
+
+	fd = open("/dev/key0", O_RDWR);
+
+	if (fd == -1) {
+		perror("open");
+		return -1;
+	}
+	
+	while (1) {
+
+		read_size = read(fd, readbuf, sizeof(readbuf));
+		
+		if (read_size < 0) {
+			perror("read");
+			return -1;	
+		}
+
+		printf("read %ld byte(s):%s\n", read_size, readbuf);
+
+	}
+
+
+
+	close(fd);
+
+	return 0;
+}
+
