974ba9198c1b0a9f5388960f199590f830013986
vijay-wang <ww107587@gmail.com>
2023-02-27 10:32:10 +0800

    test

--- icm20608/icm20608.c
+++ icm20608/icm20608.c
@@ -42,26 +42,143 @@ struct icm20608_dev {
 
 static struct icm20608_dev icm20608;
 
-//static int icm20608_read_regs(struct icm20608_dev *dev, u8 reg, u8 *data, int len )
-//{
-//        return 0;
-//}
-//
-//static unsigned char icm20608_read_reg(struct icm20608_dev *dev, u8 reg)
-//{
-//   
-//        return 0;
-//}
-//
-//static void icm20608_readdata(struct icm20608_dev *dev)
-//{
-//}
-//
+static int icm20608_write_regs(struct icm20608_dev *dev, u8 reg,
+        u8 *data, int len)
+{
+        int ret = -1;
+        unsigned char txdata[1];
+        unsigned char *rxdata;
+        struct spi_message m;
+        struct spi_transfer *t;
+        struct spi_device *spi = (struct spi_device*)dev->private_data;
+
+        t = kzalloc(sizeof(struct spi_transfer), GFP_KERNEL);
+
+        if (!t) {
+                return -ENOMEM;
+        }
+
+        rxdata = kzalloc(sizeof(char) * len, GFP_KENEL);
+
+        if(!rxdata) {
+                goto out1; 
+        }
+
+        txdata[0] = reg & ~0x80;
+        memcpy(txdata + 1, buf, len);
+        t->tx_buf = txdata;
+        t->len = len + 1;
+        spi_message_init(&m);
+        spi_message_add_tail(t, &m);
+        ret = spi_sync(spi, &m);
+
+        if (ret) {
+            goto out2;
+        }
+
+out2:
+        kfree(rxdata);
+
+out1:
+        kfree(t);
+
+
+        return ret;
+}
+
+static unsigned char icm20608_read_regs(struct icm20608_dev *dev, u8 reg,
+        void *buf, int len)
+{
+        int ret = -1;
+        unsigned char txdata[1];
+        unsigned char *rxdata;
+        struct spi_message m;
+        struct spi_transfer *t;
+        struct spi_device *spi = (struct spi_device*)dev->private_data;
+
+        t = kzalloc(sizeof(struct spi_transfer), GFP_KERNEL);
+
+        if (!t) {
+                return -ENOMEM;
+        }
+
+        rxdata = kzalloc(sizeof(char) * len, GFP_KENEL);
+
+        if(!rxdata) {
+                goto out1; 
+        }
+
+        txdata[0] = reg | 0x80;
+        t->tx_buf = txdata;
+        t->rx_buf = rxdata;
+        t->len = len + 1;
+        spi_message_init(&m);
+        spi_message_add_tail(t, &m);
+        ret = spi_sync(spi, &m);
+
+        if (ret) {
+            goto out2;
+        }
+
+        memcpy(buf, rxdata + 1, len);
+
+out2:
+        kfree(rxdata);
+
+out1:
+        kfree(t);
+
+        return ret;
+}
+
+static void icm20608_write_onereg(struct icm20608_dev *dev, u8 reg, u8 value)
+{
+    u8 buf = value;
+    icm20608_write_regs(dev, reg, &buf, 1);
+}
+
+static unsigned char icm20608_read_onereg(struct icm20608_dev *dev, u8 reg)
+{
+    u8 data = 0;
+    icm20608_read_regs(dev, reg, &data, 1);
+    return data;
+}
+
+static void icm20608_readdata(struct icm20608_dev *dev)
+{
+        unsigned char data[14] = { 0 };
+
+        icm20608_read_regs(dev, ICM20_ACCEL_XOUT_H, data, 14);
+
+        dev->gyro_x  = (signed short)((data[0] << 8) | data[1]);
+        dev->gyro_y  = (signed short)((data[2] << 8) | data[3]);
+        dev->gyro_z  = (signed short)((data[4] << 8) | data[5]);
+        dev->temp    = (signed short)((data[6] << 8) | data[7]);
+        dev->accel_x = (signed short)((data[8] << 8) | data[9]);
+        dev->accel_y = (signed short)((data[10] << 8) | data[11]);
+        dev->accel_z = (signed short)((data[12] << 8) | data[13]);
+
+}
+
 static ssize_t icm20608_read(struct file *filp, char __user *buf,
         size_t sz, loff_t *offt)
 {
+        signed int data[7];
+        struct icm20608_dev *dev = (struct icm20608_dev *)filp->private_data;
 
-        return 0;
+        icm20608_readdata(dev);
+
+        data[0] = dev->gyro_x;
+        data[1] = dev->gyro_y;
+        data[2] = dev->gyro_z;
+        data[3] = dev->accel_x;
+        data[4] = dev->accel_y;
+        data[5] = dev->accel_z;
+        data[6] = dev->temp;
+
+        copy_to_user(buf, data, sizeof(data));
+
+        return sizeof(data);
 }
 
 //static int icm20608_write_regs(struct icm20608_dev *dev, u8 reg, u8 *buf, int len)
@@ -72,6 +189,12 @@ static ssize_t icm20608_read(struct file *filp, char __user *buf,
 //static void icm20608_write_reg(struct icm20608_dev *dev, u8 reg, u8 data)
 //{
 //}
+static void icm20608_reginit(void)
+{
+    u8 value = 0;
+
+    icm20608_write_onereg(&icm20608, ICM)
+}
 
 static int icm20608_open(struct inode *node, struct file *filp)
 {
@@ -84,6 +207,11 @@ static int icm20608_open(struct inode *node, struct file *filp)
 static int icm20608_release(struct inode *node, struct file *filp)
 {
         return 0;
+}
+
+static void icm20608_reginit(void)
+{
+    
 }
 
 static struct file_operations icm20608_ops = {
@@ -132,6 +260,11 @@ static int icm20608_probe(struct spi_device *spidev)
 	if (IS_ERR(icm20608.pdevice))
 		return PTR_ERR(icm20608.pdevice);
 
+    spidev->mode = SPI_MODE_0; /*CPOL=0, CPHA=0*/
+    spidev_setup(spi);
+    icm20608.private_data = spi;
+
+    icm20608_reginit();
 
         return 0;
 }
