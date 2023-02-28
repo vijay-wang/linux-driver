91bb25a9458ad37f228fe164ead33fcbf1a6450a
vijay-wang <ww107587@gmail.com>
2023-02-28 17:56:39 +0800

    add icm20608

--- icm20608/icm20608.c
+++ icm20608/icm20608.c
@@ -1,325 +1,405 @@
 #include <linux/types.h>
-#include <linux/cdev.h>
 #include <linux/kernel.h>
 #include <linux/delay.h>
 #include <linux/ide.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/errno.h>
+#include <linux/gpio.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/of_gpio.h>
+#include <linux/semaphore.h>
+#include <linux/timer.h>
+#include <linux/i2c.h>
+#include <linux/spi/spi.h>
+#include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/of_gpio.h>
-#include <linux/gpio.h>
-#include <linux/spi/spi.h>
-#include <linux/gfp.h>
-
-#define DEV_NAME "icm20608"
-#define DEV_CNT  1
-
-/* 加速度输出 */
-#define ICM20_ACCEL_XOUT_H          0x3B
-#define ICM20_ACCEL_XOUT_L          0x3C
-#define ICM20_ACCEL_YOUT_H          0x3D
-#define ICM20_ACCEL_YOUT_L          0x3E
-#define ICM20_ACCEL_ZOUT_H          0x3F
-#define ICM20_ACCEL_ZOUT_L          0x40
-
-/* 温度输出 */
-#define ICM20_TEMP_OUT_H            0x41
-#define ICM20_TEMP_OUT_L            0x42
-
-/* 陀螺仪输出 */
-#define ICM20_GYRO_XOUT_H           0x43
-#define ICM20_GYRO_XOUT_L           0x44
-#define ICM20_GYRO_YOUT_H           0x45
-#define ICM20_GYRO_YOUT_L           0x46
-#define ICM20_GYRO_ZOUT_H           0x47
-#define ICM20_GYRO_ZOUT_L           0x48
+#include <linux/platform_device.h>
+#include <asm/mach/map.h>
+#include <asm/uaccess.h>
+#include <asm/io.h>
+#include "icm20608reg.h"
+/***************************************************************
+Copyright © ALIENTEK Co., Ltd. 1998-2029. All rights reserved.
+文件名		: icm20608.c
+作者	  	: 左忠凯
+版本	   	: V1.0
+描述	   	: ICM20608 SPI驱动程序
+其他	   	: 无
+论坛 	   	: www.openedv.com
+日志	   	: 初版V1.0 2019/9/2 左忠凯创建
+***************************************************************/
+#define ICM20608_CNT	1
+#define ICM20608_NAME	"icm20608"
 
 struct icm20608_dev {
-        dev_t devid;
-        unsigned int major;
-        unsigned int minor;
-        struct cdev cdev;
-        struct class *pclass;
-        struct device *pdevice;
-        void *private_data;
-        signed int gyro_x;
-        signed int gyro_y;
-        signed int gyro_z;
-        signed int accel_x;
-        signed int accel_y;
-        signed int accel_z;
-        signed int temp;
+	dev_t devid;				/* 设备号 	 */
+	struct cdev cdev;			/* cdev 	*/
+	struct class *class;		/* 类 		*/
+	struct device *device;		/* 设备 	 */
+	struct device_node	*nd; 	/* 设备节点 */
+	int major;					/* 主设备号 */
+	void *private_data;			/* 私有数据 		*/
+	signed int gyro_x_adc;		/* 陀螺仪X轴原始值 	 */
+	signed int gyro_y_adc;		/* 陀螺仪Y轴原始值		*/
+	signed int gyro_z_adc;		/* 陀螺仪Z轴原始值 		*/
+	signed int accel_x_adc;		/* 加速度计X轴原始值 	*/
+	signed int accel_y_adc;		/* 加速度计Y轴原始值	*/
+	signed int accel_z_adc;		/* 加速度计Z轴原始值 	*/
+	signed int temp_adc;		/* 温度原始值 			*/
 };
 
-static struct icm20608_dev icm20608;
+static struct icm20608_dev icm20608dev;
 
-static int icm20608_write_regs(struct icm20608_dev *dev, u8 reg,
-        u8 *data, int len)
+/*
+ * @description	: 从icm20608读取多个寄存器数据
+ * @param - dev:  icm20608设备
+ * @param - reg:  要读取的寄存器首地址
+ * @param - val:  读取到的数据
+ * @param - len:  要读取的数据长度
+ * @return 		: 操作结果
+ */
+static int icm20608_read_regs(struct icm20608_dev *dev, u8 reg, void *buf, int len)
 {
-        int ret = -1;
-        unsigned char txdata[1];
-        unsigned char *rxdata;
-        struct spi_message m;
-        struct spi_transfer *t;
-        struct spi_device *spi = (struct spi_device*)dev->private_data;
 
-        t = kzalloc(sizeof(struct spi_transfer), GFP_KERNEL);
+	int ret = -1;
+	unsigned char txdata[1];
+	unsigned char * rxdata;
+	struct spi_message m;
+	struct spi_transfer *t;
+	struct spi_device *spi = (struct spi_device *)dev->private_data;
+    
+	t = kzalloc(sizeof(struct spi_transfer), GFP_KERNEL);	/* 申请内存 */
+	if(!t) {
+		return -ENOMEM;
+	}
 
-        if (!t) {
-                return -ENOMEM;
-        }
+	rxdata = kzalloc(sizeof(char) * len, GFP_KERNEL);	/* 申请内存 */
+	if(!rxdata) {
+		goto out1;
+	}
 
-        rxdata = kzalloc(sizeof(char) * len, GFP_KERNEL);
-
-        if(!rxdata) {
-                goto out1; 
-        }
-
-        txdata[0] = reg & ~0x80;
-        memcpy(txdata + 1, data, len);
-        t->tx_buf = txdata;
-        t->len = len + 1;
-        spi_message_init(&m);
-        spi_message_add_tail(t, &m);
-        ret = spi_sync(spi, &m);
-
-        if (ret) {
-            goto out2;
-        }
+	/* 一共发送len+1个字节的数据，第一个字节为
+	寄存器首地址，一共要读取len个字节长度的数据，*/
+	txdata[0] = reg | 0x80;		/* 写数据的时候首寄存器地址bit8要置1 */			
+	t->tx_buf = txdata;			/* 要发送的数据 */
+    t->rx_buf = rxdata;			/* 要读取的数据 */
+	t->len = len+1;				/* t->len=发送的长度+读取的长度 */
+	spi_message_init(&m);		/* 初始化spi_message */
+	spi_message_add_tail(t, &m);/* 将spi_transfer添加到spi_message队列 */
+	ret = spi_sync(spi, &m);	/* 同步发送 */
+	if(ret) {
+		goto out2;
+	}
+	
+    memcpy(buf , rxdata+1, len);  /* 只需要读取的数据 */
 
 out2:
-        kfree(rxdata);
-
-out1:
-        kfree(t);
-
-
-        return ret;
+	kfree(rxdata);					/* 释放内存 */
+out1:	
+	kfree(t);						/* 释放内存 */
+	
+	return ret;
 }
 
-static unsigned char icm20608_read_regs(struct icm20608_dev *dev, u8 reg,
-        void *buf, int len)
+/*
+ * @description	: 向icm20608多个寄存器写入数据
+ * @param - dev:  icm20608设备
+ * @param - reg:  要写入的寄存器首地址
+ * @param - val:  要写入的数据缓冲区
+ * @param - len:  要写入的数据长度
+ * @return 	  :   操作结果
+ */
+static s32 icm20608_write_regs(struct icm20608_dev *dev, u8 reg, u8 *buf, u8 len)
 {
-        int ret = -1;
-        unsigned char txdata[1];
-        unsigned char *rxdata;
-        struct spi_message m;
-        struct spi_transfer *t;
-        struct spi_device *spi = (struct spi_device*)dev->private_data;
-
-        t = kzalloc(sizeof(struct spi_transfer), GFP_KERNEL);
-
-        if (!t) {
-                return -ENOMEM;
-        }
-
-        rxdata = kzalloc(sizeof(char) * len, GFP_KERNEL);
-
-        if(!rxdata) {
-                goto out1; 
-        }
-
-        txdata[0] = reg | 0x80;
-        t->tx_buf = txdata;
-        t->rx_buf = rxdata;
-        t->len = len + 1;
-        spi_message_init(&m);
-        spi_message_add_tail(t, &m);
-        ret = spi_sync(spi, &m);
-
-        if (ret) {
-            goto out2;
-        }
-
-        memcpy(buf, rxdata + 1, len);
-
+	int ret = -1;
+	unsigned char *txdata;
+	struct spi_message m;
+	struct spi_transfer *t;
+	struct spi_device *spi = (struct spi_device *)dev->private_data;
+	
+	t = kzalloc(sizeof(struct spi_transfer), GFP_KERNEL);	/* 申请内存 */
+	if(!t) {
+		return -ENOMEM;
+	}
+	
+	txdata = kzalloc(sizeof(char)+len, GFP_KERNEL);
+	if(!txdata) {
+		goto out1;
+	}
+	
+	/* 一共发送len+1个字节的数据，第一个字节为
+	寄存器首地址，len为要写入的寄存器的集合，*/
+	*txdata = reg & ~0x80;	/* 写数据的时候首寄存器地址bit8要清零 */
+    memcpy(txdata+1, buf, len);	/* 把len个寄存器拷贝到txdata里，等待发送 */
+	t->tx_buf = txdata;			/* 要发送的数据 */
+	t->len = len+1;				/* t->len=发送的长度+读取的长度 */
+	spi_message_init(&m);		/* 初始化spi_message */
+	spi_message_add_tail(t, &m);/* 将spi_transfer添加到spi_message队列 */
+	ret = spi_sync(spi, &m);	/* 同步发送 */
+    if(ret) {
+        goto out2;
+    }
+	
 out2:
-        kfree(rxdata);
-
+	kfree(txdata);				/* 释放内存 */
 out1:
-        kfree(t);
-
-        return ret;
+	kfree(t);					/* 释放内存 */
+	return ret;
 }
+
+/*
+ * @description	: 读取icm20608指定寄存器值，读取一个寄存器
+ * @param - dev:  icm20608设备
+ * @param - reg:  要读取的寄存器
+ * @return 	  :   读取到的寄存器值
+ */
+static unsigned char icm20608_read_onereg(struct icm20608_dev *dev, u8 reg)
+{
+	u8 data = 0;
+	icm20608_read_regs(dev, reg, &data, 1);
+	return data;
+}
+
+/*
+ * @description	: 向icm20608指定寄存器写入指定的值，写一个寄存器
+ * @param - dev:  icm20608设备
+ * @param - reg:  要写的寄存器
+ * @param - data: 要写入的值
+ * @return   :    无
+ */	
 
 static void icm20608_write_onereg(struct icm20608_dev *dev, u8 reg, u8 value)
 {
-    u8 buf = value;
-    icm20608_write_regs(dev, reg, &buf, 1);
+	u8 buf = value;
+	icm20608_write_regs(dev, reg, &buf, 1);
 }
 
-static unsigned char icm20608_read_onereg(struct icm20608_dev *dev, u8 reg)
+/*
+ * @description	: 读取ICM20608的数据，读取原始数据，包括三轴陀螺仪、
+ * 				: 三轴加速度计和内部温度。
+ * @param - dev	: ICM20608设备
+ * @return 		: 无。
+ */
+void icm20608_readdata(struct icm20608_dev *dev)
 {
-    u8 data = 0;
-    icm20608_read_regs(dev, reg, &data, 1);
-    return data;
+	unsigned char data[14] = { 0 };
+	icm20608_read_regs(dev, ICM20_ACCEL_XOUT_H, data, 14);
+
+	dev->accel_x_adc = (signed short)((data[0] << 8) | data[1]); 
+	dev->accel_y_adc = (signed short)((data[2] << 8) | data[3]); 
+	dev->accel_z_adc = (signed short)((data[4] << 8) | data[5]); 
+	dev->temp_adc    = (signed short)((data[6] << 8) | data[7]); 
+	dev->gyro_x_adc  = (signed short)((data[8] << 8) | data[9]); 
+	dev->gyro_y_adc  = (signed short)((data[10] << 8) | data[11]);
+	dev->gyro_z_adc  = (signed short)((data[12] << 8) | data[13]);
+
+    printk("===%d\n", dev->accel_x_adc);
+    printk("===%d\n", dev->accel_y_adc);
+    printk("===%d\n", dev->accel_z_adc);
+    printk("===%d\n", dev->temp_adc);
+    printk("===%d\n", dev->gyro_x_adc);
+    printk("===%d\n", dev->gyro_y_adc);
+    printk("===%d\n", dev->gyro_z_adc);
+
+
 }
 
-static void icm20608_readdata(struct icm20608_dev *dev)
+/*
+ * @description		: 打开设备
+ * @param - inode 	: 传递给驱动的inode
+ * @param - filp 	: 设备文件，file结构体有个叫做pr似有ate_data的成员变量
+ * 					  一般在open的时候将private_data似有向设备结构体。
+ * @return 			: 0 成功;其他 失败
+ */
+static int icm20608_open(struct inode *inode, struct file *filp)
 {
-        unsigned char data[14] = { 0 };
-
-        icm20608_read_regs(dev, ICM20_ACCEL_XOUT_H, data, 14);
-
-        dev->gyro_x  = (signed short)((data[0] << 8) | data[1]);
-        dev->gyro_y  = (signed short)((data[2] << 8) | data[3]);
-        dev->gyro_z  = (signed short)((data[4] << 8) | data[5]);
-        dev->temp    = (signed short)((data[6] << 8) | data[7]);
-        dev->accel_x = (signed short)((data[8] << 8) | data[9]);
-        dev->accel_y = (signed short)((data[10] << 8) | data[11]);
-        dev->accel_z = (signed short)((data[12] << 8) | data[13]);
-
+	filp->private_data = &icm20608dev; /* 设置私有数据 */
+	return 0;
 }
 
-static ssize_t icm20608_read(struct file *filp, char __user *buf,
-        size_t sz, loff_t *offt)
+/*
+ * @description		: 从设备读取数据 
+ * @param - filp 	: 要打开的设备文件(文件描述符)
+ * @param - buf 	: 返回给用户空间的数据缓冲区
+ * @param - cnt 	: 要读取的数据长度
+ * @param - offt 	: 相对于文件首地址的偏移
+ * @return 			: 读取的字节数，如果为负值，表示读取失败
+ */
+static ssize_t icm20608_read(struct file *filp, char __user *buf, size_t cnt, loff_t *off)
 {
-        signed int data[7];
-        int ret;
-        struct icm20608_dev *dev = (struct icm20608_dev *)filp->private_data;
-        printk("===========2");
+	signed int data[7];
+	long err = 0;
+	struct icm20608_dev *dev = (struct icm20608_dev *)filp->private_data;
 
-        icm20608_readdata(dev);
-
-        data[0] = dev->gyro_x;
-        data[1] = dev->gyro_y;
-        data[2] = dev->gyro_z;
-        data[3] = dev->accel_x;
-        data[4] = dev->accel_y;
-        data[5] = dev->accel_z;
-        data[6] = dev->temp;
-
-        printk("===========1");
-        ret = copy_to_user(buf, data, sizeof(data));
-
-        return sizeof(data);
+	icm20608_readdata(dev);
+	data[0] = dev->gyro_x_adc;
+	data[1] = dev->gyro_y_adc;
+	data[2] = dev->gyro_z_adc;
+	data[3] = dev->accel_x_adc;
+	data[4] = dev->accel_y_adc;
+	data[5] = dev->accel_z_adc;
+	data[6] = dev->temp_adc;
+	err = copy_to_user(buf, data, sizeof(data));
+	return 0;
 }
 
-static void icm20608_reginit(void)
+/*
+ * @description		: 关闭/释放设备
+ * @param - filp 	: 要关闭的设备文件(文件描述符)
+ * @return 			: 0 成功;其他 失败
+ */
+static int icm20608_release(struct inode *inode, struct file *filp)
 {
-        unsigned char who_am_i;
-
-        icm20608_write_onereg(&icm20608, 0x6b, 0x80);
-        mdelay(100);
-        icm20608_write_onereg(&icm20608, 0x6b, 0x01);
-        mdelay(100);
-
-        icm20608_write_onereg(&icm20608, 0x19, 0x00);                /*输出速率是内部采样率                */
-        icm20608_write_onereg(&icm20608, 0x1b, 0x18); // 设置陀螺仪量程为2000dps
-        icm20608_write_onereg(&icm20608, 0x1c, 0x18); // 设置加速度计量程为16g)))
-
-        icm20608_write_onereg(&icm20608, 0x1a, 0x04);        /* 陀螺仪低通滤波BW=20Hz                */
-        icm20608_write_onereg(&icm20608, 0x1d, 0x04); /* 加速度计低通滤波BW=21.2Hz            */
-        icm20608_write_onereg(&icm20608, 0x6c, 0x00);    /* 打开加速度计和陀螺仪所有轴               */
-        icm20608_write_onereg(&icm20608, 0x1e, 0x00);   /* 关闭低功耗                       */
-        icm20608_write_onereg(&icm20608, 0x23, 0x00);       /* 关闭FIFO                     */
-
-        // 读取WHO_AM_I寄存器
-        who_am_i = icm20608_read_onereg(&icm20608, 0x75);
-        printk(KERN_INFO "ICM20608 WHO_AM_I = 0x%x\n", who_am_i);
-
+	return 0;
 }
 
-static int icm20608_open(struct inode *node, struct file *filp)
-{
-        filp->private_data = &icm20608; 
-        printk("open icm20608\n");
-
-        return 0;
-}
-
-static int icm20608_release(struct inode *node, struct file *filp)
-{
-        return 0;
-}
-
-static struct file_operations icm20608_ops = {
-	.owner   = THIS_MODULE,
-	.read    = icm20608_read,
-	.open    = icm20608_open,
-        .release = icm20608_release,
+/* icm20608操作函数 */
+static const struct file_operations icm20608_ops = {
+	.owner = THIS_MODULE,
+	.open = icm20608_open,
+	.read = icm20608_read,
+	.release = icm20608_release,
 };
 
+/*
+ * ICM20608内部寄存器初始化函数 
+ * @param  	: 无
+ * @return 	: 无
+ */
+void icm20608_reginit(void)
+{
+	u8 value = 0;
+	
+	icm20608_write_onereg(&icm20608dev, ICM20_PWR_MGMT_1, 0x80);
+	mdelay(50);
+	icm20608_write_onereg(&icm20608dev, ICM20_PWR_MGMT_1, 0x01);
+	mdelay(50);
+
+	value = icm20608_read_onereg(&icm20608dev, ICM20_WHO_AM_I);
+	printk("ICM20608 ID = %#X\r\n", value);	
+
+	icm20608_write_onereg(&icm20608dev, ICM20_SMPLRT_DIV, 0x00); 	/* 输出速率是内部采样率					*/
+	icm20608_write_onereg(&icm20608dev, ICM20_GYRO_CONFIG, 0x18); 	/* 陀螺仪±2000dps量程 				*/
+	icm20608_write_onereg(&icm20608dev, ICM20_ACCEL_CONFIG, 0x18); 	/* 加速度计±16G量程 					*/
+	icm20608_write_onereg(&icm20608dev, ICM20_CONFIG, 0x04); 		/* 陀螺仪低通滤波BW=20Hz 				*/
+	icm20608_write_onereg(&icm20608dev, ICM20_ACCEL_CONFIG2, 0x04); /* 加速度计低通滤波BW=21.2Hz 			*/
+	icm20608_write_onereg(&icm20608dev, ICM20_PWR_MGMT_2, 0x00); 	/* 打开加速度计和陀螺仪所有轴 				*/
+	icm20608_write_onereg(&icm20608dev, ICM20_LP_MODE_CFG, 0x00); 	/* 关闭低功耗 						*/
+	icm20608_write_onereg(&icm20608dev, ICM20_FIFO_EN, 0x00);		/* 关闭FIFO						*/
+}
+
+ /*
+  * @description     : spi驱动的probe函数，当驱动与
+  *                    设备匹配以后此函数就会执行
+  * @param - client  : i2c设备
+  * @param - id      : i2c设备ID
+  * 
+  */	
+static int icm20608_probe(struct spi_device *spi)
+{
+	/* 1、构建设备号 */
+	if (icm20608dev.major) {
+		icm20608dev.devid = MKDEV(icm20608dev.major, 0);
+		register_chrdev_region(icm20608dev.devid, ICM20608_CNT, ICM20608_NAME);
+	} else {
+		alloc_chrdev_region(&icm20608dev.devid, 0, ICM20608_CNT, ICM20608_NAME);
+		icm20608dev.major = MAJOR(icm20608dev.devid);
+	}
+
+	/* 2、注册设备 */
+	cdev_init(&icm20608dev.cdev, &icm20608_ops);
+	cdev_add(&icm20608dev.cdev, icm20608dev.devid, ICM20608_CNT);
+
+	/* 3、创建类 */
+	icm20608dev.class = class_create(THIS_MODULE, ICM20608_NAME);
+	if (IS_ERR(icm20608dev.class)) {
+		return PTR_ERR(icm20608dev.class);
+	}
+
+	/* 4、创建设备 */
+	icm20608dev.device = device_create(icm20608dev.class, NULL, icm20608dev.devid, NULL, ICM20608_NAME);
+	if (IS_ERR(icm20608dev.device)) {
+		return PTR_ERR(icm20608dev.device);
+	}
+
+	/*初始化spi_device */
+	spi->mode = SPI_MODE_0;	/*MODE0，CPOL=0，CPHA=0*/
+	spi_setup(spi);
+	icm20608dev.private_data = spi; /* 设置私有数据 */
+
+	/* 初始化ICM20608内部寄存器 */
+	icm20608_reginit();		
+	return 0;
+}
+
+/*
+ * @description     : i2c驱动的remove函数，移除i2c驱动的时候此函数会执行
+ * @param - client 	: i2c设备
+ * @return          : 0，成功;其他负值,失败
+ */
+static int icm20608_remove(struct spi_device *spi)
+{
+	/* 删除设备 */
+	cdev_del(&icm20608dev.cdev);
+	unregister_chrdev_region(icm20608dev.devid, ICM20608_CNT);
+
+	/* 注销掉类和设备 */
+	device_destroy(icm20608dev.class, icm20608dev.devid);
+	class_destroy(icm20608dev.class);
+	return 0;
+}
+
+/* 传统匹配方式ID列表 */
 static const struct spi_device_id icm20608_id[] = {
-        { "alientek,icm20608", 0 },
-        { }
+	{"alientek,icm20608", 0},  
+	{}
 };
 
-/*有设备树方式匹配表*/
+/* 设备树匹配列表 */
 static const struct of_device_id icm20608_of_match[] = {
-        { .compatible = "alientek,icm20608" },
-        { }
+	{ .compatible = "alientek,icm20608" },
+	{ /* Sentinel */ }
 };
 
-static int icm20608_probe(struct spi_device *spidev)
-{
-        if (alloc_chrdev_region(&icm20608.devid, 0, 1, DEV_NAME)) {
-                printk("alloc_chrdev_region: allocate devid faild\r\n");
-                return -1;
-        }
-
-        icm20608.major = MAJOR(icm20608.devid);
-        icm20608.minor = MINOR(icm20608.devid);
-
-        printk("major:%d, minor:%d\r\n", icm20608.major, icm20608.minor);
-
-        cdev_init(&icm20608.cdev, &icm20608_ops);
-        if (cdev_add(&icm20608.cdev, icm20608.devid, 1)) {
-                printk("cdev_init: init dev faild\r\n");
-                return -1;
-        }
-
-        icm20608.pclass = class_create(THIS_MODULE, DEV_NAME);
-
-	if(IS_ERR(icm20608.pclass))
-		return PTR_ERR(icm20608.pclass);
-
-        icm20608.pdevice = device_create(icm20608.pclass, NULL, icm20608.devid, NULL, DEV_NAME);
-
-	if (IS_ERR(icm20608.pdevice))
-		return PTR_ERR(icm20608.pdevice);
-
-    spidev->mode = SPI_MODE_0; /*CPOL=0, CPHA=0*/
-    spi_setup(spidev);
-    icm20608.private_data = spidev;
-
-    icm20608_reginit();
-
-        return 0;
-}
-
-static int icm20608_remove(struct spi_device *spidev)
-{
-        cdev_del(&icm20608.cdev);
-        unregister_chrdev_region(icm20608.devid, 1);
-        device_destroy(icm20608.pclass, icm20608.devid);
-        class_destroy(icm20608.pclass);
-        return 0;
-}
-
+/* SPI驱动结构体 */	
 static struct spi_driver icm20608_driver = {
-        .probe  = icm20608_probe,
-        .remove = icm20608_remove,
-        .driver = {
-                .owner = THIS_MODULE,
-                .name  = "icm20608",
-                .of_match_table = icm20608_of_match,
-        },
-        .id_table = icm20608_id,
+	.probe = icm20608_probe,
+	.remove = icm20608_remove,
+	.driver = {
+			.owner = THIS_MODULE,
+		   	.name = "icm20608",
+		   	.of_match_table = icm20608_of_match, 
+		   },
+	.id_table = icm20608_id,
 };
-
-static int icm20608_init(void)
+		   
+/*
+ * @description	: 驱动入口函数
+ * @param 		: 无
+ * @return 		: 无
+ */
+static int __init icm20608_init(void)
 {
-        return spi_register_driver(&icm20608_driver);
+	return spi_register_driver(&icm20608_driver);
 }
 
-static void icm20608_exit(void)
+/*
+ * @description	: 驱动出口函数
+ * @param 		: 无
+ * @return 		: 无
+ */
+static void __exit icm20608_exit(void)
 {
-        spi_unregister_driver(&icm20608_driver);
+	spi_unregister_driver(&icm20608_driver);
 }
 
 module_init(icm20608_init);
 module_exit(icm20608_exit);
-MODULE_AUTHOR("Wenjie Wang <ww107587@gmail.com>");
-MODULE_DESCRIPTION("AP3216 Driver");
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("zuozhongkai");
+
+
+
