ee5eda1de73d90ede25c77fc8f3479c9f16df416
vijay-wang <ww107587@gmail.com>
2023-04-21 14:24:59 +0800

    add iio_i2c_ap3216c

---
+++ iio_i2c_ap3216c/ap3216c.h
@@ -0,0 +1,16 @@
+#ifndef __AP3216C_H
+#define __AP3216C_H
+
+#define
+
+#define SYSTEM_CONFIG	0x00
+#define IR_DATA_LOW  	0x0A
+#define IR_DATA_HIGH 	0x0B
+#define ALS_DATA_LOW 	0x0C
+#define ALS_DATA_HIGH	0x0D
+#define PS_DATA_LOW  	0x0E
+#define PS_DATA_HIGH 	0x0F
+
+
+
+#endif
